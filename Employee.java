23d52dd797eaac9b16164b36d9ca451eefff93c3
chithra0831 <chithrasm0831@gmail.com>
2025-05-07 14:09:18 +0530

    second

--- Employee.java
+++ Employee.java
@@ -2,11 +2,13 @@ public class Employee {
     String name;
     int id;
     double salary;
+    int age;
     
-    public Employee(String name,int id,double salary){
+    public Employee(String name,int id,double salary,int age){
         this.name = name;
         this.id = id;
         this.salary = salary;
+        this.age=age;
 
     }
     public void displayInfo() {
@@ -14,9 +16,11 @@ public class Employee {
         System.out.println("Emloyee ID: " + id);
         System.out.println("Emloyee Name: " + name);
         System.out.println("Salary: " + salary);
+        System.out.println("Age: " + age);
+
     }
     public static void main(String[] args){
-        Employee emp1 = new Employee("xyz",101,50000.0);
+        Employee emp1 = new Employee("xyz",101,50000.0,35);
         emp1.displayInfo();
     }
 
